645f8d860558b443615e0041202bbf64980c720f
Samuel Cabral <samfcabral@gmail.com>
2015-03-19 10:53:00 -0700

    fix to lower vs upper case

--- letterCounter.js
+++ letterCounter.js
@@ -2,6 +2,8 @@ angular.module('letterCounter', [])
   .controller('letterCtrl', ['$scope', function ($scope) {
     $scope.countLetters = function (str) {
       var result = {};
+      var str = str.toUpperCase();
+      console.log("This is upper case " + str);
       for (var i = 0, char = str[i]; i < str.length; i += 1, char = str[i]) {
         result[char] = (result[char] || 0) + 1;
         console.log(result);
