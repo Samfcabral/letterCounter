d1362fc6740273a65d7a2bfb80c473d390f072eb
Samuel Cabral <samfcabral@gmail.com>
2015-03-18 20:11:13 -0700

    Initial setup

---
+++ letterCounter.js
@@ -0,0 +1,13 @@
+angular.module('letterCounter', [])
+  .controller('letterCtrl', ['$scope', function($scope) {
+    $scope.countLetters = function (str) {
+      var result = {};
+      for (var i = 0, char = str[i]; i < str.length; i += 1, char = str[i]) {
+        result[char] = (result[char] || 0) + 1;
+        console.log(result);
+      }
+      $scope.result = result;
+      console.log(result);
+      return result;
+    };
+  }])
